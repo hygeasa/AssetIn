3ddb1f78a2bddbcb6b4cacc71dce90ea685ab3a4
Hygea Saveria <starla@Hygeas-MacBook-Air.local>
2023-10-28 20:25:29 +0700

    Add asset and color

---
+++ AssetIn/AssetIn/Common/Resource/Color+Default.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Default.swift
+//  AssetIn
+//
+//  Created by Hygea Saveria on 28/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    enum AssetIn {
+        static let orange = Color("orange")
+        static let bg = Color("bg")
+        static let green = Color("green")
+        static let grey = Color("grey")
+        static let yellow = Color("yellow")
+        static let purple = Color("purple")
+        
+    }
+}
---
+++ AssetIn/AssetIn/Common/Resource/Image+Default.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  AssetIn
+//
+//  Created by Hygea Saveria on 28/10/23.
+//
+
+import SwiftUI
+
+extension Image {
+    static let logoAssetin = Image("logo-assetin")
+    static let logoWithText = Image("logo-with-text")
+}
