79f4839e613cdaa01c4a289507a13443a4f253ef
hygeasa <hygeasa@gmail.com>
2023-12-11 22:40:49 +0700

    create change password view

---
+++ AssetIn/AssetIn/Feature/ChangePassword/View/ChangePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordView.swift
+//  AssetIn
+//
+//  Created by Hygea Saveria on 07/12/23.
+//
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordView()
+}
---
+++ AssetIn/AssetIn/Feature/ChangePassword/ViewModel/ChangePasswordViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordViewModel.swift
+//  AssetIn
+//
+//  Created by Hygea Saveria on 07/12/23.
+//
+
+import SwiftUI
+
+struct ChangePasswordViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordViewModel()
+}
